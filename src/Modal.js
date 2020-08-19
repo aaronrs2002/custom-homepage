cbb8e7bdda5a230376fddc0ad1b197cc3db86850
aaronrs2002 <aaronrs2002@yahoo.com>
2020-08-19 06:39:16 -0700

    update

--- src/Modal.js
+++ src/Modal.js
@@ -12,7 +12,7 @@ class Modal extends Component {
       {
         title: "Calendar",
         content:
-          "<iframe src='https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=MONTH&amp;height=600&amp;wkst=1&amp;bgcolor=%23cccccc&amp;src=aaronrs2002%40gmail.com&amp;color=%23333333&amp;src=%23contacts%40group.v.calendar.google.com&amp;color=%232F6309&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23691426&amp;ctz=America%2FPhoenix' id='calendarHere'/>"
+          "<iframe src='https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=MONTH&amp;height=600&amp;wkst=1&amp;bgcolor=%23cccccc&amp;src=XXXXXXXX&amp;color=%23333333&amp;src=%23contacts%40group.v.calendar.google.com&amp;color=%232F6309&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23691426&amp;ctz=America%2FPhoenix' id='calendarHere'/>"
       }
     ];
     return (
