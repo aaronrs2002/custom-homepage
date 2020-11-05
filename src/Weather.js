7f80554944356bf202ae6974e621b4f5d1f5a68d
aaronrs2002 <aaronrs2002@yahoo.com>
2020-11-05 07:05:23 -0700

    updated config

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DateTime from "./DateTime";
+import WeatherAPI from "./config/WeatherAPI.js";
 
 class Weather extends Component {
   constructor(props) {
@@ -8,46 +9,48 @@ class Weather extends Component {
       weatherLoaded: false,
       theWeather: [],
       timezone_id: "America/Phoenix",
-      currentZip: this.props.zipCode
+      currentZip: this.props.zipCode,
     };
     this.grabWeather = this.grabWeather.bind(this);
     this.submitZip = this.submitZip.bind(this);
   }
 
   grabWeather(zipCode) {
-    fetch(
-      "XXXXXX/yahooWeather.php?zip=" +
-        zipCode
-    )
-      .then(res => res.text())
+    fetch(WeatherAPI + zipCode)
+      .then((res) => res.text())
       .then(
-        result => {
+        (result) => {
           this.setState({
             weatherLoaded: true,
-            theWeather: JSON.parse(result)
+            theWeather: JSON.parse(result),
             //weatherData: result
           });
           if (this.state.theWeather.length > 0) {
             this.setState({
-              timezone_id: this.state.theWeather.location.timezone_id
+              timezone_id: this.state.theWeather.location.timezone_id,
             });
           }
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
-        error => {
+        (error) => {
           this.setState({
-            weatherLoaded: true,
-            error
+            weatherLoaded: false,
+            error,
           });
         }
       );
   }
 
   componentWillMount() {
-    console.log("this.props.zipCode: " + this.props.zipCode);
     this.grabWeather(this.props.zipCode);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.zipCode !== prevProps.zipCode) {
+      this.grabWeather(this.props.zipCode);
+    }
   }
 
   submitZip() {
@@ -57,13 +60,15 @@ class Weather extends Component {
 
     console.log("zipCode match: " + zipQualify);
     if (zipCode.length >= 5 && zipQualify === true) {
-      this.setState({
-        currentZip: zipCode
-      });
-      this.grabWeather(zipCode);
-
-      console.log("zipCode: " + zipCode);
-      document.querySelector("#zipCode").value = "";
+      this.setState(
+        {
+          currentZip: zipCode,
+        },
+        () => {
+          this.grabWeather(zipCode);
+          this.props.updateZipCode();
+        }
+      );
     } else {
       console.log("Does not qualify as a zip code.");
     }
@@ -74,7 +79,7 @@ class Weather extends Component {
       <div className="row paddBottom">
         <div className="col-md-12">
           <ul className="inline weather">
-            {this.state.weatherLoaded
+            {this.state.weatherLoaded === true
               ? this.state.theWeather.forecasts.map((date, i) => {
                   if (i <= 6) {
                     return (
@@ -93,53 +98,57 @@ class Weather extends Component {
                     );
                   }
                 })
-              : "no weather"}
+              : null}
           </ul>
         </div>
-        <div className="col-md-4 centerTxt">
-          {this.state.weatherLoaded
-            ? this.state.theWeather.location.city +
-              ", " +
-              this.state.theWeather.location.region +
-              " " +
-              this.state.currentZip
-            : "no weather"}
-        </div>
-        <div className="col-md-4">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search weather by US zip code"
-              id="zipCode"
-            />
-            <span className="input-group-btn">
-              <button
-                className="btn btn-secondary"
-                onClick={this.submitZip}
-                type="button"
-              >
-                Go
-                <i className="icon-white icon-search" />
-              </button>
-            </span>
+        {this.state.weatherLoaded === true ? (
+          <div className="col-md-4 centerTxt">
+            {this.state.weatherLoaded
+              ? this.state.theWeather.location.city +
+                ", " +
+                this.state.theWeather.location.region +
+                " " +
+                this.props.zipCode
+              : "no weather"}
           </div>
-        </div>
-
-        <div className="col-md-4">
-          {this.state.weatherLoaded ? (
-            <DateTime
-              tz={this.state.theWeather.location.timezone_id}
-              zipCode={this.state.currentZip}
-            />
-          ) : (
-            "no weather"
-          )}
-        </div>
+        ) : null}
+        {this.state.weatherLoaded === true ? (
+          <div className="col-md-4">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search weather by US zip code"
+                id="zipCode"
+              />
+              <span className="input-group-btn">
+                <button
+                  className="btn btn-secondary"
+                  onClick={this.submitZip}
+                  type="button"
+                >
+                  Go
+                  <i className="icon-white icon-search" />
+                </button>
+              </span>
+            </div>
+          </div>
+        ) : null}
+        {this.state.weatherLoaded === true ? (
+          <div className="col-md-4">
+            {this.state.weatherLoaded ? (
+              <DateTime
+                tz={this.state.theWeather.location.timezone_id}
+                zipCode={this.state.currentZip}
+              />
+            ) : (
+              "no weather"
+            )}
+          </div>
+        ) : null}
       </div>
     );
   }
 }
 
 export default Weather;
-
